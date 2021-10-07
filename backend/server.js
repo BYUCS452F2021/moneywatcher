53d6c75021f3ff25ac770493e7bb148478c3c6bf
smith897 <skyler.smith897@gmail.com>
2021-10-07 17:26:17 -0600

    Add read_all for budgets table
    And fix some typos

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ let db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READWRITE, (err) 
     console.log('Connected to the in-memory SQlite database.');
 });
 // Uncomment this line to create the table
-// db.run('CREATE TABLE budget(categoryID numbereger primary key autoincrement, Name varchar(255), Amount double);');
+// db.run('CREATE TABLE budget(categoryID number primary key autoincrement, Name varchar(255), Amount double);');
 
 
 /**
@@ -48,7 +48,7 @@ app.post('/budget/create', (req, res) => {
     stmt.run([name, amount], function (err) {
         if (err) {
             console.error(err);
-            res.status(500).json({message: "Error adding to database"});
+            res.status(500).json({message: "Error adding to budget table in database"});
         } else {
             res.status(200).json({categoryID: this.lastID});
         }
@@ -76,14 +76,36 @@ app.post('/budget/create', (req, res) => {
     db.get(sql, [categoryID], (err, row) => {
         if (err) {
             console.error(err);
-            res.status(500).json({message: "Error reading from database"});
+            res.status(500).json({message: "Error reading from budget table in database"});
         } else {
-        row == undefined
-            ? console.log(`No category found with the id ${categoryID}`)
-            : console.log(`Returned budget with categoryId ${categoryID}`);
-          res.status(200).json({result: row});
+            row == undefined
+                ? console.log(`No category found with the id ${categoryID}`)
+                : console.log(`Returned budget with categoryId ${categoryID}`);
+            res.status(200).json({result: row});
         }
       });
+
+});
+
+/**
+ * Read all rows row in the budget table.
+ */
+ app.post('/budget/read_all', (req, res) => {
+    // (No need to verify request)
+
+    // Execute the query
+    let sql = `SELECT * FROM budget`;
+    db.all(sql, [], (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({message: "Error reading all from budget table in database"});
+        } else {
+            data == undefined
+                ? console.log(`No data found in the budget table`)
+                : console.log(`Returned entire budget table`);
+            res.status(200).json({result: data});
+            }
+        });
 
 });
 
@@ -114,7 +136,7 @@ app.post('/budget/create', (req, res) => {
     db.run(sql, [name, amount, categoryID], function(err) {
         if (err) {
             console.error(err);
-            res.status(500).json({message: "Error updating database"});
+            res.status(500).json({message: "Error updating budget table in database"});
         }
         console.log(`Row(s) updated: ${this.changes}`);
         res.status(200).json({message: "Updated succesfully"});
@@ -140,13 +162,13 @@ app.post('/budget/create', (req, res) => {
     db.run(sql, [categoryID], function(err) {
         if (err) {
             console.error(err);
-            res.status(500).json({message: "Error deleting from database"});
+            res.status(500).json({message: "Error deleting from budget table in database"});
         }
         res.status(200).json({message: "Deleted succesfully"});
       });
 });
 
+// Start the server
 app.listen(5000, () => {
-    // Verify request format
     console.log('Server listening on port 5000');
 });
