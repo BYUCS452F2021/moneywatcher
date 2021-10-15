5ae8353d38379f0ff1adfb67ac2bba855b921c53
smith897 <skyler.smith897@gmail.com>
2021-10-15 12:08:58 -0600

    Add button
    Need to centertext

--- frontend/moneywatcher/src/views/Home.vue
+++ frontend/moneywatcher/src/views/Home.vue
@@ -10,6 +10,11 @@
       <div class="row">
         <input class="item3" v-model="expense_description" placeholder="Description">
       </div>
+      <div class="centered-row">
+        <div class="horiz-spacer2"/>
+        <button class="item4">Add</button>
+        <div class="horiz-spacer2"/>
+      </div>
     </div>
   </div>
 </template>
@@ -50,6 +55,20 @@ export default {
   }
 
   .item3 {
+    display: flex;
+    flex: 1;
+  }
+
+  .centered-row {
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+  }
+  .horiz-spacer2 {
+    display: flex;
+    flex: 4;
+  }
+  .item4 {
     display: flex;
     flex: 1;
   }
