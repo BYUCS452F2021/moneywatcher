b61f396af3bbd110d4bc64ea063b2a7dc94257e6
smith897 <skyler.smith897@gmail.com>
2021-10-27 17:45:57 -0600

    Get a basic chart showing

---
+++ frontend/moneywatcher/src/components/Chart.vue
@@ -0,0 +1,29 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+// TODO: Figure out how the things below create the chart, make API calls to get the data, and display it
+
+export default {
+  extends: Bar,
+  data: () => ({
+    chartdata: {
+      labels: ['January', 'February'],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: [40, 0]
+        },
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
