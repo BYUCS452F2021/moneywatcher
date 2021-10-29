55d859894c039fbf217d85c4acddc17b247eba95
smith897 <skyler.smith897@gmail.com>
2021-10-28 19:42:32 -0600

    Get chart working with API data
    Not thoroughly tested, has an ugly color

--- frontend/moneywatcher/src/components/Chart.vue
+++ frontend/moneywatcher/src/components/Chart.vue
@@ -7,38 +7,66 @@ import axios from "../main";
 export default {
   extends: Bar,
   data: () => ({
+    apidata: {
+      categories: [],
+    },
     chartdata: {
       labels: [],
       datasets: [
         {
-          label: 'Food',
           backgroundColor: '#f87979',
-          data: [40, 0]
-        },
-        {
-          label: 'Food',
-          backgroundColor: '#f87979',
-          data: [40, 0]
+          data: [40, 15, 30]
         },
       ]
     },
     options: {
       responsive: true,
-      maintainAspectRatio: false
+      maintainAspectRatio: false,
+      legend: {
+        display: false,
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              min: 0,
+              max: 100,
+              beginAtZero: true,
+            }
+          }
+        ]
+      }
     }
   }),
 
   mounted () {
     axios.get('/budget/read_all').then((budgetResponse) => {
       var result = budgetResponse.data.result;
-      this.chartdata.labels = [];
+      this.apidata.categories = [];
       result.forEach((category) => {
-        this.chartdata.labels.push(category.Name);
+        this.apidata.categories.push({name: category.Name, totalAmount: category.Amount, currentAmount: 0});
       });
-      axios.post('/expenses/read_all_names_by_date').then((expensesResponse) => {
-          console.log(expensesResponse)
+      axios.post('/expenses/read_all_names_by_date', {
+        month: 10,
+        year: 2021
+      }).then((expensesResponse) => {
+          expensesResponse.data.result.forEach((expense) => {
+            this.apidata.categories.forEach((category) => {
+              if (category.name === expense.categoryName) {
+                category.currentAmount += expense.Amount;
+              }
+            })
+          })
+
+          this.chartdata.labels = this.apidata.categories.map((category) => category.name);
+          this.chartdata.datasets[0].data = [];
+          this.apidata.categories.forEach((category) => {
+            this.chartdata.datasets[0].data.push(100 * category.currentAmount / category.totalAmount);
+          })
+          console.log(this.chartdata.datasets);
+          console.log(this.apidata.categories);
+          this.renderChart(this.chartdata, this.options)
       });
-      this.renderChart(this.chartdata, this.options)
     });
   }
 }
