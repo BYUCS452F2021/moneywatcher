df2dee9d8415465b9c20b9ffcf06aa0145f55148
Cullen <cullensteig@gmail.com>
2021-12-07 13:55:37 -0700

    Frontend fixes to make date sorting work

--- frontend/moneywatcher/src/views/Review.vue
+++ frontend/moneywatcher/src/views/Review.vue
@@ -121,7 +121,13 @@ export default {
   methods: {
     dateChanged(date) {
       this.updateDate(date);
-      this.updateExpensesByDate();
+      if (date.month != null) {
+        this.updateExpensesByDate();
+      }
+      else {
+          this.updateExpenses();
+      }
+      
     },
     dateCleared() {
       this.date = null;
@@ -163,24 +169,24 @@ export default {
           month: monthToGet,
           year: this.date.year,
       }).then((response) => {
-        var result = response.data.result;
+        var result = response.data;
         var newExpenses = [];
         result.forEach((expense) => {
-            var date = new Date(parseInt(expense.Day));
+            var date = new Date(parseInt(expense.day));
             var dateString = new Date(date).toDateString();
             dateString = dateString.substr(dateString.indexOf(" ") + 1);
-            var description = expense.Description;
+            var description = expense.description;
             if (description.length === 0) {
                 description = "No description given.";
             }
           newExpenses.push({
-            expenseID: expense.expenseID,
+            expenseID: expense._id,
             date: date,
             dateString: dateString,
-            amount: parseFloat(expense.Amount).toFixed(2),
+            amount: parseFloat(expense.amount).toFixed(2),
             description: description,
-            vendorName: expense.vendorName,
-            categoryName: expense.categoryName
+            vendorName: expense.vendor,
+            categoryName: expense.budget[0].Name,
           });
         });
         this.expenses = newExpenses;
